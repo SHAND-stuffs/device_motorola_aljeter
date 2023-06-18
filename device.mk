44980d1e1faa4655e6ea0cd097e31fd2e109d828
SH4ND <miqueiasantos20@gmail.com>
2023-06-18 18:59:33 -0300

    aljeter: Create script to remove apps on build
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- device.mk
+++ device.mk
@@ -95,6 +95,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_product_strategies.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_product_strategies.xml \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_stream_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_stream_volumes.xml
 
+# Remove packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Camera
 PRODUCT_PACKAGES += \
     libstdc++.vendor \
