8062427a0facc86a92996f318944ebf49a4fcd4e
SH4ND <miqueiasantos20@gmail.com>
2023-11-23 17:45:18 -0300

    aljeter: Move powerhint.xml to powerhint path
    Change-Id: I56a3bd2f9451690d2832c6a72425985eff39b2f5

--- device.mk
+++ device.mk
@@ -264,7 +264,7 @@ TARGET_NO_PREBUILTS := true
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QMI
 PRODUCT_PACKAGES += \
